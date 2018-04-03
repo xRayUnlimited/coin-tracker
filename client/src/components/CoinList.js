6c663286a0aca27dab1106ccba26a41e97600900
Ray Suyoto <ray.suy@icloud.com>
2018-04-03 11:59:02 -0600

    component update

--- client/src/components/CoinList.js
+++ client/src/components/CoinList.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { 
+  Table, 
+  Header, 
+  Divider 
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { getCoins } from '../actions/coins';
 
-const CoinList = () => <div></div>
+class CoinList extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getCoins())
+  }
 
-export default CoinList;
+  render() {
+    const { coins } = this.props;
+    return (
+      <div>
+        <Divider />
+        <Table celled striped>
+          <Table.Header>
+            { ['Symbol', 'Name', 'Price']
+                .map( h =>
+                  <Table.HeaderCell key={h}>
+                    {h}
+                  </Table.HeaderCell>
+                )
+            }
+          </Table.Header>
+          <Table.Body>
+            { coins.map( coin => {
+                const { price, name, symbol, id, cmc_id } = coin
+                return (
+                  <Table.Row key={id}>
+                    <Table.Cell>
+                      <Link to={`/coins/${cmc_id}`}>
+                        { symbol }
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>
+                      { name }
+                    </Table.Cell>
+                    <Table.Cell>
+                      ${ price }
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              })
+            }
+          </Table.Body>
+        </Table>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { coins: state.coins }
+}
+
+export default connect(mapStateToProps)(CoinList);
