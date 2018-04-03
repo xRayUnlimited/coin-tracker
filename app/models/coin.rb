3741e9fbde1f0abd0cb4af92b68f5befa1807914
Ray Suyoto <ray.suy@icloud.com>
2018-04-03 12:00:43 -0600

    uh

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -6,17 +6,17 @@ class Coin < ApplicationRecord
   has_many :users, through: :watched_coins
 
   def self.create_by_cmc_id(res)
-    if /^2\d\d$/ =~ res.code.to_s
+    if /^2\d\d$/ =~ res.code.to_s 
       match = res[0].with_indifferent_access
       coin_params = {
-        name: match[:name],
-        symbol: match[:symbol],
-        cmc_id: match[:id]
+        name: match[:name], 
+        symbol: match[:symbol], 
+        cmc_id: match[:id] 
       }
 
       Coin.find_or_create_by(coin_params) do |coin|
         coin.price = match[:price_usd]
-        coin.last_fetched  DateTime.now
+        coin.last_fetched = DateTime.now
       end
     else
       nil
