40f15d97814c123e498a2c1544251acb1cfeabca
Ray Suyoto <ray.suy@icloud.com>
2018-04-03 10:58:09 -0600

    server side

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :watched_coins, dependent: :destroy
-  has_many :coins, through: :watched_coins
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :watched_coins, dependent: :destroy
+  has_many :coins, through: :watched_coins
 end
